3ffd16c06155f41f7592f788c78753a0f7e9e658
刘春牢 <liuchunlao@qq.com>
2016-05-20 16:38:45 +0800

    添加水平的控制器

---
+++ MDRCustomLayout/Classes/Controllers/MDRLinerController.h
@@ -0,0 +1,13 @@
+//
+//  MDRLinerController.h
+//  MDRCustomLayout
+//
+//  Created by 刘春牢 on 16/5/20.
+//  Copyright © 2016年 liuchunlao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDRLinerController : UIViewController
+
+@end
---
+++ MDRCustomLayout/Classes/Controllers/MDRLinerController.m
@@ -0,0 +1,31 @@
+//
+//  MDRLinerController.m
+//  MDRCustomLayout
+//
+//  Created by 刘春牢 on 16/5/20.
+//  Copyright © 2016年 liuchunlao. All rights reserved.
+//
+
+#import "MDRLinerController.h"
+
+@interface MDRLinerController ()
+
+@end
+
+@implementation MDRLinerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"LinerScale";
+    
+    // MARK: - collectionView
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
