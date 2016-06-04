59bbb75088ecf00c2d38a19f49c35c6b2ccef45e
刘春牢 <liuchunlao@qq.com>
2016-06-05 01:12:36 +0800

    录屏准备工作

---
+++ MDRCustomLayout/Classes/Record/Controllers/RecordScreen/MDRRecordScreenController.h
@@ -0,0 +1,13 @@
+//
+//  MDRRecordScreenController.h
+//  MDRCustomLayout
+//
+//  Created by 刘春牢 on 16/6/5.
+//  Copyright © 2016年 liuchunlao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDRRecordScreenController : UIViewController
+
+@end
---
+++ MDRCustomLayout/Classes/Record/Controllers/RecordScreen/MDRRecordScreenController.m
@@ -0,0 +1,29 @@
+//
+//  MDRRecordScreenController.m
+//  MDRCustomLayout
+//
+//  Created by 刘春牢 on 16/6/5.
+//  Copyright © 2016年 liuchunlao. All rights reserved.
+//
+
+#import "MDRRecordScreenController.h"
+
+@interface MDRRecordScreenController ()
+
+@end
+
+@implementation MDRRecordScreenController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
